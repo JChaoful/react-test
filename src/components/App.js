aa080869ff6bb55818a6624c077e58a603ba8054
jchao03 <jchao03@uw.edu>
2023-04-18 23:59:28 -0700

    user code updated (can check history for original)

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,7 @@ function App() {
   const [currentBoard, setCurrentBoard] = useState(0);
   const [boardsDisplayed, setBoardsDisplayed] = useState([true, false, false]);
   const [boardContents, setBoardContents] = useState([["/pinboard-0-initial-image.jpg"],["/pinboard-1-initial-image.jpg"],["/pinboard-2-initial-image.jpg"]]);
-  const [dragOverTimer, setDragOverTimer] = useState(0);
-  const [dragTarget, setDragTarget] = useState(null);
 
-  const DRAGOVER_SWITCH_DELAY = 1000;
 
   function openBoard(index) {
     const newBoardsDisplayed = new Array(boardsDisplayed.length).fill(false);
@@ -18,58 +15,16 @@ function App() {
     setCurrentBoard(index);
   }
 
-  const handleDragStart = (e) => {
-    e.target.style.opacity=0.75;
-    setDragTarget(e.target);
-  }
-
-  const handleDragEnd = (e) => {
-    e.target.style.opacity=1.0;
-    setDragTarget(null);
-  }
 
   const handleLeftClick = () => {
     const prevBoardIndex = Math.max(0, currentBoard - 1);
     openBoard(prevBoardIndex);
   }
 
-  const handleLeftDragOver = () => {
-    if (dragOverTimer === 0) {
-      setDragOverTimer(Date.now());
-    } else if (Date.now() - dragOverTimer > DRAGOVER_SWITCH_DELAY) {
-      setDragOverTimer(Date.now() - 100);
-      const prevBoardIndex = Math.max(0, currentBoard - 1);
-      openBoard(prevBoardIndex);
-    }
-  }
 
   const handleRightClick = () => {
     const nextBoardIndex = Math.min(boardsDisplayed.length - 1, currentBoard + 1);
     openBoard(nextBoardIndex);
-  }
-
-  const handleRightDragOver = () => {
-    if (dragOverTimer === 0) {
-      setDragOverTimer(Date.now());
-    } else if (Date.now() - dragOverTimer > DRAGOVER_SWITCH_DELAY) {
-      setDragOverTimer(Date.now() -  100);
-      const nextBoardIndex = Math.min(boardsDisplayed.length - 1, currentBoard + 1);
-      openBoard(nextBoardIndex);
-    }
-  }
-
-  const handleArrowDragleave = () => {
-    setDragOverTimer(0);
-  }
-
-  const handleBoardDrop = (e) => {
-    if (dragTarget !== null) {
-      const newBoardContents = boardContents;
-      const updatedBoard = boardContents[currentBoard];
-      updatedBoard.push(dragTarget.src);
-      newBoardContents[currentBoard] = updatedBoard;
-      setBoardContents(newBoardContents);
-    }
   }
 
   return (
@@ -77,14 +32,14 @@ function App() {
       <h1>Pinboard App</h1>
       <div className="searched-images">
         <p className="label">Searched Images: </p>
-        <img src="/sample-image.jpg" className="sample-image" alt="cat from search result" draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}/>
+        <img src="/sample-image.jpg" className="sample-image" alt="cat from search result"/>
       </div>
-      <div className="pinboard-carousel" onDrop={handleBoardDrop}>
-        <i className="fa-solid fa-chevron-left chevron" onClick={handleLeftClick} onDragOver={handleLeftDragOver} onDragLeave={handleArrowDragleave}/>
+      <div className="pinboard-carousel">
+        <i className="fa-solid fa-chevron-left chevron" onClick={handleLeftClick}/>
         {
         boardsDisplayed.map((showBoard, index) => 
-          <Pinboard showBoard={showBoard} content={boardContents[index]} boardIndex={index} key={index} handleDragStart={handleDragStart} handleDragEnd={handleDragEnd} boardContents={boardContents} setBoardContents={setBoardContents}/>)}
-        <i className="fa-solid fa-chevron-right chevron" onClick={handleRightClick} onDragOver={handleRightDragOver} onDragLeave={handleArrowDragleave}/>
+          <Pinboard showBoard={showBoard} content={boardContents[index]} boardIndex={index} key={index} boardContents={boardContents} setBoardContents={setBoardContents}/>)}
+        <i className="fa-solid fa-chevron-right chevron" onClick={handleRightClick}/>
       </div>
       <footer>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
--- src/components/Pinboard.js
+++ src/components/Pinboard.js
@@ -1,25 +1,11 @@
-function Pinboard({showBoard, content, boardIndex, handleDragStart, handleDragEnd, boardContents, setBoardContents}) {
-    const handleBoardDragEnd = (e) => {
-        handleDragEnd(e);
-        const newBoardContents = boardContents;
-        const updatedBoard = boardContents[boardIndex];
-        const deletedIndex = updatedBoard.indexOf(e.target.src);
-        updatedBoard.splice(deletedIndex, 1);
-        newBoardContents[boardIndex] = updatedBoard;
-        setBoardContents(newBoardContents);
-    }
-    
+function Pinboard({showBoard, content, boardIndex, boardContents, setBoardContents}) { 
     const images = content.map((src, index) => 
-        <img src={src} key={index} className="pinboard-image" alt="pinboard-originated" draggable onDragStart={handleDragStart} onDragEnd={handleBoardDragEnd}/>
+        <img src={src} key={index} className="pinboard-image" alt="pinboard-originated"/>
     );
-
-    const handleBoardDragOver = (e) => {
-        e.preventDefault();
-    }
 
     return (
         <div className="board-container">
-            <div style={{display: showBoard ? "flex" : "none"}} className={`board board-index-${boardIndex}`} onDragOver={handleBoardDragOver}>
+            <div style={{display: showBoard ? "flex" : "none"}} className={`board board-index-${boardIndex}`}>
             <p className="label">Current Pinboard: {boardIndex}</p>
             <div className="images">
                 {images}
